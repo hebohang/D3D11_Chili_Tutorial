6fb370087321914263fe23861a17aaedf390935d
hebohang <hebohang1113@gmail.com>
2022-01-23 20:13:30 +0800

    3：消息循环

--- D3D11_Chili_Tutorial/WinMain.cpp
+++ D3D11_Chili_Tutorial/WinMain.cpp
@@ -1,5 +1,18 @@
 #include <Windows.h>
 
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_CLOSE:
+		PostQuitMessage(69);  // 把退出消息送往消息队列，自定义 69 代表关闭应用进程
+		break;
+	default:
+		break;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
@@ -13,7 +26,7 @@ int CALLBACK WinMain(
 	WNDCLASSEX wc = { 0 };
 	wc.cbSize = sizeof(wc);
 	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = DefWindowProc;
+	wc.lpfnWndProc = WndProc;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
@@ -38,6 +51,22 @@ int CALLBACK WinMain(
 
 	// show the damn window
 	ShowWindow(hWnd, SW_SHOW);
-	while (true);
-	return 0;
+
+	// message pump
+	MSG msg;
+	BOOL gResult;  // 这里的 BOOL 实际上是一个 int 型，typedef int BOOL，而非 C++ 的关键字 bool
+	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	if (gResult == -1)
+	{
+		return -1;
+	}
+	else
+	{
+		return msg.wParam;
+	}
 }
